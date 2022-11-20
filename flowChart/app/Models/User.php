222e0b20b453fb3a6400b59158b307d4b57a5769
kieran <kieranfarrerdev@gmail.com>
2022-11-20 13:58:41 +0000

    add uuid package to User Model

--- flowChart/app/Models/User.php
+++ flowChart/app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Dyrynda\Database\Support\GeneratesUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -20,7 +21,10 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, 
+        HasFactory, 
+        Notifiable, 
+        GeneratesUuid;
 
     /**
      * The attributes that are mass assignable.
@@ -41,14 +45,5 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }
